cf5d58c7c2cf985a77e003eb9ce0ba85313b52c4
ZWho1963 <zwho1963@gmail.com>
2023-12-07 16:43:35 +1100

    Add Coyote Jump

--- player.gd
+++ player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 class_name Player
 
+@onready var coyote_jump_timer = $CoyoteJumpTimer
 @export var SPEED = 250.0
 @export var ACCELERATION = 1500.0
 @export var FRICTION = 1500.0
@@ -9,25 +10,27 @@ class_name Player
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 @export var gravity = 980
 
-
 func _physics_process(delta):
 	apply_gravity(delta)
 	handle_jump()
 	var input_axis = Input.get_axis("ui_left", "ui_right")
 	handle_acceleration(input_axis, delta)
 	apply_friction(input_axis, delta)
-
+	var was_on_floor = is_on_floor()
 	move_and_slide()
+	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0 
+	if just_left_ledge:
+		coyote_jump_timer.start()
 	
 func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 			
 func handle_jump():
-	if velocity.y == 0 and is_on_floor():
+	if velocity.y == 0 and is_on_floor() or coyote_jump_timer.time_left > 0.0:
 		if Input.is_action_just_pressed("ui_accept"):
 			velocity.y = JUMP_VELOCITY
-	else:
+	if not is_on_floor():
 		if Input.is_action_just_released("ui_accept") and velocity.y < JUMP_VELOCITY / 2:
 			velocity.y = JUMP_VELOCITY / 2
 		
@@ -38,4 +41,5 @@ func apply_friction(input_axis, delta):
 func handle_acceleration(input_axis, delta):
 	if input_axis != 0:
 		velocity.x = move_toward(velocity.x, SPEED * input_axis, ACCELERATION * delta)
+
 
